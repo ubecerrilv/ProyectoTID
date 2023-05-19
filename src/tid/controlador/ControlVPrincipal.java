aa2f053431c91961ec996f64fbd4380b451182da
markjs010 <mgomezv010@gmail.com>
2023-05-19 18:18:40 -0500

    switch en ControlVP creado

--- src/tid/controlador/ControlVPrincipal.java
+++ src/tid/controlador/ControlVPrincipal.java
@@ -6,7 +6,10 @@ public class ControlVPrincipal extends ControlAbs {
 
 	@Override
 	public Data ejecutaComando(String c, Data d, Data d2) {//EN ESTA CLASE, REALIZAR TRABAJOS DEL MODELO
-			
+		switch(c) {
+		case Comandos.ECUALIZAR:
+			break;
+		}
 		return null;//REGRESAR UN MODELO
 	}
 
